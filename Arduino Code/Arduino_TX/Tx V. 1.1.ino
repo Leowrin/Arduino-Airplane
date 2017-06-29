d3cf7dd2501776214d4d6732441a194411d4d2dd
Leowrin <leo.dana.30@gmail.com>
2017-06-29 16:37:34 +0200

    Merge branch 'master' of https://github.com/Leowrin/Drone

--- Arduino Code/Arduino_TX/Tx V. 1.1.ino
+++ Arduino Code/Arduino_TX/Tx V. 1.1.ino
@@ -9,15 +9,8 @@ String RightM;
 String LeftM;
 
 
-
-
-
-
-
-
 void setup() {
 Serial.begin(9600);
-
 
 
 comserial.begin(9600);
@@ -31,44 +24,28 @@ Pitch = String("A:");
 Pitch = String(Pitch+pitch);
 Serial.print(Pitch);
 
-Serial.print("\t");
 //B
 int lefta = map(analogRead(A1),0,1023,0,99);
 LeftA = String("B:");
 LeftA = String(LeftA+lefta);
 Serial.print(LeftA);
 
-Serial.print("\t");
 //C
-int righta = map(analogRead(A1),0,1023,0,99);
-RightA = String("C:");
-RightA = String(RightA+righta);
-Serial.print(RightA);
-
-Serial.print("\t");
-//D
 int yaw = map(analogRead(A3),0,1023,0,99);
 Yaw = String("D:");
 Yaw = String(Yaw+yaw);
 Serial.print(Yaw);
 
-Serial.print("\t");
-//E
+//D
 int rightm = map(analogRead(A6),0,1023,0,99);
 RightM = String("E:");
 RightM = String(RightM+rightm);
 Serial.print(RightM);
 
-Serial.print("\t");
-//F
+//E
 int leftm = map(analogRead(A7),0,1023,0,99);
 LeftM = String("F:");
 LeftM = String(LeftM+leftm);
 Serial.print(LeftM);
 
-
-
-//sc=String(sv);
-//comserial.print(sc);
-Serial.println();
 }
