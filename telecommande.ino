41ba680d12966b860491286a41ec7685124fa355
Leowrin <leo.dana.30@gmail.com>
2018-04-07 17:21:58 +0200

    .

--- telecommande.ino
+++ telecommande.ino
@@ -1,146 +1,94 @@
+#include <Servo.h>
+#define bps 2400
+#include <SoftwareSerial.h>
+//Déclaration des servos juteux.
+Servo SPitch;
+Servo SRightA;
+Servo SLeftA;
+Servo SYaw;
+Servo SLESC;
+Servo SRESC;
+
+//Variables
 boolean comstate;
-int pitch;
-int lefta;
-int righta;
-int yaw;
-int lesc;
-int resc;
+int Pitch;
+int RightA;
+int LeftA;
+int Yaw;
+int LESC;
+int RESC;
 long timeA;
 long timeB;
 long timeC;
-float lescm;
-float rescm;
 
 
-String entry;
 String data;
+String entryA;
 
 
+SoftwareSerial mySerial(2, 3); //RX, TX
 void setup() {
+//Serial
+Serial.begin(bps);
+Serial.setTimeout(40);
+mySerial.begin(bps);
+mySerial.setTimeout(40);
 
-  Serial.begin(2400);     //2400 [baud/sec] pour une meilleure portée
-  Serial.setTimeout(40);
-  delay(2000);          //attente du condo
+//servo.attach
+/*SPitch.attach(3);
+SRightA.attach(9);
+SLeftA.attach(10);
+SYaw.attach(11);
+SLESC.attach(5);
+SRESC.attach(6);*/
+
 
 }
-
 
 void loop() {
-//Assiniation des variables, remises à 0.
-data="";
-entry="";
-timeA=0;
-timeB=0;
-timeC=0;
+  //var set
+  data="";
 
-//pitch, A
-//Faire Monter / Déscendre
-pitch = analogRead(A0);
-if (pitch < 10) {
-  data = String(data+"000"+pitch);
-} else if (pitch < 100) {
-  data = String(data+"00"+pitch);
-} else if (pitch < 1000) {
-  data = String(data+"0"+pitch);
-} else {
-  data = String(data+pitch);
-}
 
-//leftaileron, B
-lefta = analogRead(A1);
-if (lefta < 10) {
-  data = String(data+"000"+lefta);
-} else if (lefta < 100) {
-  data = String(data+"00"+lefta);
-} else if (lefta < 1000) {
-  data = String(data+"0"+lefta);
-} else {
-  data = String(data+lefta);
-}
+if(mySerial.available() > 1){
+   data = mySerial.readString();
 
-//yaw
-if (digitalRead(4)==HIGH){
-  delay(1);
-  if (digitalRead(4)==HIGH){
-    data = String(data+"0000");
-}} else if (digitalRead(2)==HIGH){
-  delay(1);
-  if (digitalRead(2)==HIGH){
-    data = String(data+"1023");
-  }} else {
-  data=String(data+"0512");
-}
+   entryA = data.substring(0,4);
+   Pitch = entryA.toInt();
 
-//moteurs
-lescm = analogRead(A6);  //A6 pot gauche
-rescm = analogRead(A7);  //A7 pot droite
-lescm=lescm/1023;
-rescm=rescm/1023;
-lesc = lescm*analogRead(A3);
-resc = rescm*analogRead(A3);
+   entryA = data.substring(4,8);
+   LeftA = entryA.toInt();
 
-if (lesc < 10) {
-  data = String(data+"000"+lesc);
-} else if (lesc < 100) {
-  data = String(data+"00"+lesc);
-} else if (lesc < 1000) {
-  data = String(data+"0"+lesc);
-} else {
-  data = String(data+lesc);
-}
+   entryA = data.substring(8,12);
+   Yaw = entryA.toInt();
 
-if (resc < 10) {
-  data = String(data+"000"+resc);
-} else if (resc < 100) {
-  data = String(data+"00"+resc);
-} else if (resc < 1000) {
-  data = String(data+"0"+resc);
-} else {
-  data = String(data+resc);
-}
+   entryA = data.substring(12,16);
+   LESC = entryA.toInt();
 
-Serial.print(data);
-delay(100);
+   entryA = data.substring(16,20);
+   RESC = entryA.toInt();
+ }
 
+
+
+
+
+ Pitch=map(Pitch,0,1023,0,1023);
+ LeftA=map(LeftA,0,1023,0,1023);
+ RightA=map(LeftA,0,1023,0,1023);
+ Yaw=map(Yaw,0,1023,0,1023);
+ LESC=map(LESC,0,1023,0,0);
+ RESC=map(RESC,0,1023,0,0);
 /*
+ SPitch.write(Pitch);
 
+ SLeftA.write(LeftA);
 
+ SRightA.write(RightA);
 
+ SYaw.write(Yaw);
 
-//delay(200); //replaced with next lines
-timeA = millis();
-while(Serial.available()<=1){
-  timeB = millis();
-  timeC = timeB-timeA;
-  if (timeC>150){
-    break;
-  }timeC=0;
-}if (Serial.available()>1){
-  entry = Serial.readString();
-  comstate=true;
-  //data process, soon batterie voltage display
-  timeA = millis();
-  timeB = 0;
-  while(timeC<100){
-    timeB = millis();
-    timeC = timeB-timeA;
-  }
-}else{
-  comstate=false;
-  //data process, soon alarm ?
-  timeA = millis();
-  timeB = 0;
-  while(timeC<100){
-    timeB = millis();
-    timeC = timeB-timeA;
-  }
-}
-switch (comstate) {
-  case 1:
-  digitalWrite(7,HIGH);
-  break;
-  case 0:
-  digitalWrite(7,LOW);
-  break;
-}*/
+ SLESC.write(RESC);
+
+ SRESC.write(LESC);*/
 }
